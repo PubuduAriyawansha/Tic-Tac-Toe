003ffc448cd5ed91cbc312057d6b786b568ede42
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-19 12:31:56 +0530

    commit4

--- TicTacToe/src/main/java/lk/ijse/dep11/MainFormController.java
+++ TicTacToe/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,9 +1,12 @@
 package lk.ijse.dep11;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +19,25 @@ public class MainFormController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public static Node getNode( int row, int column, GridPane gridPane){
+        Node result = null;
+        ObservableList<Node> children = gridPane.getChildren();
+        for(Node node : children){
+            if(gridPane.getRowIndex(node)==row && gridPane.getColumnIndex(node)==column){
+                result=node;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public String getURL (String name){
+        File file = new File(name);
+        String absolute = file.getAbsolutePath();
+        absolute=absolute.substring(0,absolute.length()-name.length());
+        return "file:\\"+absolute+"src\\main\\resources\\view\\images\\"+name;
     }
 
 
