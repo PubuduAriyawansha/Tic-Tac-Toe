27ca99601a481323cb53276cd6306af443ea3bbd
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-19 11:10:02 +0530

    commit1

---
+++ TicTacToe/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class MainFormController {
+    public Label txtHead;
+    public GridPane grid;
+}
